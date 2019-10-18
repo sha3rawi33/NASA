957f77f0fd00eef65ef08e398282f71c7740156b
Elctrony <48838991+Elctrony@users.noreply.github.com>
2019-10-18 14:09:35 +0200

    edied

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ANything edited',
+      title: 'ANything ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
