b9090633e8d262364758e28e850c77236826867a
Elctrony <48838991+Elctrony@users.noreply.github.com>
2019-10-18 13:47:23 +0200

    Nasa
    NASA SDGs

---
+++ lib/HomePage.dart
@@ -0,0 +1,250 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sdgs/RankingScreen.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isSelected = true;
+
+  Drawer getNavDrawer(BuildContext context) {
+    var aboutChild = AboutListTile(
+        child: prefix.Text("About"),
+        applicationName: "Application Name",
+        applicationVersion: "v1.0.0",
+        applicationIcon: Icon(Icons.adb),
+        icon: Icon(Icons.info));
+
+    var myNavChildren = [
+      Container(
+        height: 200,
+        width: double.infinity,
+        color: Colors.blue,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              CircleAvatar(
+                child: Icon(
+                  Icons.account_circle,
+                  size: 60,
+                ),
+                radius: 30,
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              Container(
+                height: 60,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 4,),
+                    prefix.Text(
+                      'Mohamed Khaled',
+                      style: TextStyle(
+                        fontFamily: 'MontserratMedium',
+                        fontSize: 20,
+                        color: const Color.fromRGBO(38, 38, 47, 1),
+                      ).apply(fontSizeDelta: -2, color: Colors.white),
+                    ),
+                    SizedBox(height: 4,),
+                    prefix.Text(
+                      '250 points',
+                      style: TextStyle(
+                        fontFamily: 'MontserratMedium',
+                        fontSize: 16,
+                        color: const Color.fromRGBO(38, 38, 47, 1),
+                      ).apply(fontSizeDelta: -2, color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      ListTile(
+        title: prefix.Text('Air Poluttion'),
+        leading: Icon(FontAwesomeIcons.airbnb),
+      ),
+      ListTile(
+        title: prefix.Text('Water Poluttion'),
+        leading: Icon(FontAwesomeIcons.water),
+      ),
+      ListTile(
+        title: prefix.Text('Water Chemical'),
+        leading: Icon(FontAwesomeIcons.airbnb),
+      ),
+      ListTile(
+        title: prefix.Text('Plastic'),
+        leading: Icon(Icons.nature_people),
+      ),
+      aboutChild
+    ];
+
+    return Drawer(
+        child: Column(
+      children: myNavChildren,
+    ));
+  }
+
+  void _dialogFlowResponse(query) async {
+    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/chat-boot.json").build();
+    Dialogflow dialogflow = Dialogflow(authGoogle: authGoogle,language: Language.english);
+    AIResponse response = await dialogflow.detectIntent("Hi!!!");
+    print('respon'+response.getMessage());
+    print('respond:'+response.toString());
+    print('respond2:'+response.queryResult.toString());
+    print('respond4:'+response.queryResult.fulfillmentMessages.toString());
+    print('respond5:'+response.queryResult.fulfillmentText);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: prefix.Text('SDGs')),
+      drawer: getNavDrawer(context),
+      body: RankingScreen(),
+      bottomNavigationBar: Row(
+        children: <Widget>[
+          _BottomNavigationButton(
+            'assets/flare/TasksIcon.flr',
+            label: 'Actions',
+            tap: () => _dialogFlowResponse('SDGs'),
+            isSelected: !isSelected,
+            hasNotification: isSelected,
+            iconSize: const Size(24, 25),
+            padding: const EdgeInsets.only(top: 15),
+          ),
+          _BottomNavigationButton(
+            'assets/flare/TeamIcon.flr',
+            label: 'Ranking',
+            tap: () => isSelected = !isSelected,
+            isSelected: isSelected,
+            hasNotification: !isSelected,
+            iconSize: const Size(25, 29),
+            padding: const EdgeInsets.only(top: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomNavigationButton extends StatefulWidget {
+  final bool hasNotification;
+  final String flare;
+  final Size iconSize;
+  final bool isSelected;
+  final EdgeInsets padding;
+  final VoidCallback tap;
+  final String label;
+
+  const _BottomNavigationButton(this.flare,
+      {this.isSelected = false,
+      this.hasNotification = false,
+      this.padding = const EdgeInsets.only(top: 15),
+      this.iconSize = const Size(25, 29),
+      this.tap,
+      this.label});
+
+  @override
+  __BottomNavigationButtonState createState() =>
+      __BottomNavigationButtonState();
+}
+
+class __BottomNavigationButtonState extends State<_BottomNavigationButton> {
+  /// We use this variable as a way to determine if this is the first time the
+  /// button is being shown.
+  /// If that's the case, we simply pop to the last frame of the animation
+  /// instead of playing it through.
+  /// This prevents all of the bottom navigation buttons from playing an
+  /// 'intro' animation when the screen is first displayed.
+  bool _isFirstShow = true;
+
+  @override
+  void didUpdateWidget(_BottomNavigationButton oldWidget) {
+    setState(() {
+      _isFirstShow = false;
+    });
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        type: MaterialType.canvas,
+        color: widget.isSelected
+            ? const Color.fromRGBO(48, 48, 59, 1)
+            : const Color.fromRGBO(38, 38, 47, 1),
+        child: InkWell(
+          onTap: widget.tap,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Stack(
+                overflow: Overflow.visible,
+                children: [
+                  Padding(
+                    padding: widget.padding,
+                    child: SizedBox(
+                      width: widget.iconSize.width,
+                      height: widget.iconSize.height,
+                      child: FlareActor(widget.flare,
+                          alignment: Alignment.center,
+                          shouldClip: false,
+                          fit: BoxFit.contain,
+                          snapToEnd: _isFirstShow,
+                          animation: widget.isSelected ? 'active' : 'inactive'),
+                    ),
+                  ),
+                  Positioned(
+                    top: -20,
+                    right: -15,
+                    child: widget.hasNotification
+                        ? const SizedBox(
+                            width: 31,
+                            height: 31,
+                            child: FlareActor(
+                                'assets/flare/NotificationIcon.flr',
+                                alignment: Alignment.center,
+                                shouldClip: false,
+                                fit: BoxFit.contain,
+                                animation: 'appear'),
+                          )
+                        : Container(),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: 5, bottom: MediaQuery.of(context).padding.bottom + 10),
+                child: prefix.Text(
+                  widget.label,
+                  style: TextStyle(
+                    fontFamily: 'MontserratMedium',
+                    fontSize: 16,
+                    color: const Color.fromRGBO(38, 38, 47, 1),
+                  ).apply(
+                    fontSizeDelta: -2,
+                    color: widget.isSelected
+                        ? Colors.white
+                        : Colors.white.withOpacity(0.35),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/SingupPage.dart
@@ -0,0 +1,380 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import './RoundedClipper.dart';
+
+class SignupAppbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.20,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.20,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.deepPurple[300],
+            ),
+            clipper: RoundedClipper(60),
+          ),
+          ClipPath(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.18,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.deepPurpleAccent,
+            ),
+            clipper: RoundedClipper(50),
+          ),
+          Positioned(
+              top: -50,
+              left: -30,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.15,
+                width: MediaQuery.of(context).size.height * 0.15,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                        (MediaQuery.of(context).size.height * 0.15) / 2),
+                    color: Colors.deepPurple[300].withOpacity(0.3)),
+                child: Center(
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.deepPurpleAccent),
+                  ),
+                ),
+              )),
+          Positioned(
+              top: -50,
+              left: MediaQuery.of(context).size.width * 0.6,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.20,
+                width: MediaQuery.of(context).size.height * 0.20,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                        (MediaQuery.of(context).size.height * 0.20) / 2),
+                    color: Colors.deepPurple[300].withOpacity(0.3)),
+                child: Center(
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.deepPurpleAccent),
+                  ),
+                ),
+              )),
+          Positioned(
+              top: -50,
+              left: 80,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.15,
+                width: MediaQuery.of(context).size.height * 0.15,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                        (MediaQuery.of(context).size.height * 0.15) / 2),
+                    color: Colors.deepPurple[300].withOpacity(0.3)),
+              )),
+          Container(
+            margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.15 - 50),
+            height: MediaQuery.of(context).size.height * 0.33,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  static const errors = <String, String>{
+    "PASSWORD_NO_MATCH": "Passwords don't match!",
+    "ERROR_WEAK_PASSWORD": "Passowrd is weak! Please input a stronger one",
+    "ERROR_INVALID_EMAIL": "Email is invalid",
+    "ERROR_EMAIL_ALREADY_IN_USE":
+        "Email is signed up with a different account!",
+    "ERROR_WRONG_PASSWORD": "Passowrd is wrong",
+    "ERROR_USER_NOT_FOUND": "The email is not associated with any user",
+    "ERROR_USER_DISABLED": "User is disabled",
+    "ERROR_TOO_MANY_REQUESTS": "Too many requests by user",
+    "ERROR_OPERATION_NOT_ALLOWED": "Operation not allowed"
+  };
+  FocusNode focusNode1;
+  FocusNode focusNode2;
+  FocusNode focusNode3;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _nameContoller = TextEditingController();
+  TextEditingController _emailContoller = TextEditingController();
+  TextEditingController _passwordContoller = TextEditingController();
+  TextEditingController _confrimPasswordContoller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    focusNode1 = FocusNode();
+    focusNode2 = FocusNode();
+    focusNode3 = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // Clean up the focus node when the Form is disposed
+    focusNode1.dispose();
+    focusNode2.dispose();
+    focusNode3.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: false,
+      body: Column(
+        children: <Widget>[
+          SignupAppbar(),
+          Builder(
+            builder: (context) => Container(
+              width: MediaQuery.of(context).size.width,
+              height: (MediaQuery.of(context).size.height * 0.80) - 22,
+              margin: EdgeInsets.fromLTRB(20, 12, 20, 10),
+              child: Form(
+                key: _formKey,
+                autovalidate: _autoValidate,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Enter name';
+                        }
+                      },
+                      controller: _nameContoller,
+                      keyboardType: TextInputType.text,
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        labelText: "Name",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical:
+                                MediaQuery.of(context).size.height * 0.022,
+                            horizontal: 15.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                      ),
+                      onFieldSubmitted: (String value) {
+                        FocusScope.of(context).requestFocus(focusNode1);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      validator: validateEmail,
+                      controller: _emailContoller,
+                      focusNode: focusNode1,
+                      keyboardType: TextInputType.emailAddress,
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical:
+                                MediaQuery.of(context).size.height * 0.022,
+                            horizontal: 15.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                      ),
+                      onFieldSubmitted: (String value) {
+                        FocusScope.of(context).requestFocus(focusNode2);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      validator: validatePassword,
+                      controller: _passwordContoller,
+                      focusNode: focusNode2,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                      textInputAction: TextInputAction.next,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical:
+                                MediaQuery.of(context).size.height * 0.022,
+                            horizontal: 15.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                      ),
+                      onFieldSubmitted: (String value) {
+                        FocusScope.of(context).requestFocus(focusNode3);
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      validator: validatePassword,
+                      controller: _confrimPasswordContoller,
+                      focusNode: focusNode3,
+                      obscureText: true,
+                      keyboardType: TextInputType.text,
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        labelText: "Confirm Password",
+                        contentPadding: new EdgeInsets.symmetric(
+                            vertical:
+                                MediaQuery.of(context).size.height * 0.022,
+                            horizontal: 15.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      child: GestureDetector(
+                          onTap: () async {
+                            Scaffold.of(context).showSnackBar(SnackBar(
+                              content: Text("Processing..."),
+                            ));
+                            _validateInputs();
+
+                            try {
+                              var password = _passwordContoller.text;
+                              var confirm = _confrimPasswordContoller.text;
+
+                              if (password != confirm)
+                                throw PlatformException(
+                                    code: "PASSWORD_NO_MATCH");
+
+                              AuthResult result = await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: _emailContoller.text,
+                                      password: password);
+                              await Firestore.instance
+                                  .collection('profiles')
+                                  .document(result.user.uid)
+                                  .setData({
+                                'name': _nameContoller.text,
+                                'email': _emailContoller.text,
+                                'score': 0
+                              });
+                              Scaffold.of(context).showSnackBar(SnackBar(
+                                content: Text(
+                                    "Successfully signed up! Check your email for activation!"),
+                              ));
+                            } on PlatformException catch (e) {
+                              Scaffold.of(context).showSnackBar(
+                                  SnackBar(content: Text(errors[e.code])));
+                            }
+                          },
+                          child: Container(
+                            height: MediaQuery.of(context).size.height * 0.065,
+                            decoration: BoxDecoration(
+                                color: Colors.deepPurpleAccent,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25))),
+                            child: Center(
+                              child: Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 16),
+                              ),
+                            ),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      child: GestureDetector(
+                          onTap: () {
+                            print("pressed");
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  color: Colors.deepPurpleAccent,
+                                  shape: BoxShape.circle),
+                              child:
+                                  Icon(Icons.arrow_back, color: Colors.white))),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  bool _value1 = false;
+  bool _autoValidate = false;
+
+  void _value1Changed(bool value) => setState(() => _value1 = value);
+
+  void _validateInputs() {
+    if (_formKey.currentState.validate()) {
+//    If all data are correct then save data to out variables
+      _formKey.currentState.save();
+    } else {
+//    If all data are not valid then start auto validation.
+      setState(() {
+        _autoValidate = true;
+      });
+    }
+  }
+
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'Password must be atleast 6 digits';
+    else
+      return null;
+  }
+
+  void signUp() async {}
+}
